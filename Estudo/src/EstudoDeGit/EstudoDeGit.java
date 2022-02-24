a618f705bc3c90ca504f40685c4e529ab0b688a1
Rafael Oliveira <87864373+jrafaeltol@users.noreply.github.com>
2022-02-24 12:34:28 -0300

    Criação de Pacote e Commit.

---
+++ Estudo/src/EstudoDeGit/EstudoDeGit.java
@@ -0,0 +1,42 @@
+package EstudoDeGit;
+
+import java.util.Scanner;
+
+public class EstudoDeGit {
+
+	
+	
+	
+		public static void main(String[] args) {
+
+			int nVezes = 0;
+			int numero = 0;
+			int countImpar = 0;
+			int countPar = 0;
+
+			Scanner scan = new Scanner(System.in);
+
+			System.out.println("Informe quantos numeros");
+			nVezes = scan.nextInt();
+
+			int count = 0;
+
+			do {
+				System.out.println("Numero: ");
+				numero = scan.nextInt();
+				count++;
+
+				if (numero % 2 == 0)
+					countPar++;
+				else
+					countImpar++;
+
+			} while (count < nVezes);
+
+			System.out.println("Você informou " + countPar + " número(s) pares");
+			System.out.println("Você informou " + countImpar + " número(s) impares");
+
+		}
+	}
+
+
