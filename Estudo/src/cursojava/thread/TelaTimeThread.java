43984977fda5b5f764af3aeb754c8d7b1687a8eb
Rafael <Rafael@DESKTOP-I649GD5>
2022-03-16 23:26:36 -0300

    Criando tela grafica

---
+++ Estudo/src/cursojava/thread/TelaTimeThread.java
@@ -0,0 +1,101 @@
+package cursojava.thread;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JInternalFrame.JDesktopIcon;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class TelaTimeThread extends JDialog {
+	
+	
+	private JPanel jpanel = new JPanel(new GridBagLayout());/*Painel de componentes*/
+	
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread 1");
+	private JTextField mostraTempo2 = new JTextField();
+	
+	private JButton jbutton = new JButton("Start");
+	private JButton jbutton1 = new JButton("Stop");
+	
+	
+	
+	
+	
+	
+	
+	public TelaTimeThread() {
+		
+		setTitle("Minha tela de Time com Thread");
+		setSize(new Dimension(240 , 240));   //defini tamanho
+		setLocationRelativeTo(null);         //defini posição inicial da tela
+		setResizable(false);                 //impede a tela de ser redimensionada
+		
+		
+		/*Primeira Parte Concluida*/
+		
+		/*Controlador de posicionamento de componentes*/
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth =2;
+		gridBagConstraints.insets =new Insets(5,5 ,5,5);
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		
+		
+		
+		
+		
+		descricaoHora.setPreferredSize(new Dimension(200 ,25));
+		jpanel.add(descricaoHora,gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200,25));
+		gridBagConstraints.gridy++;
+		jpanel.add(mostraTempo, gridBagConstraints);
+		mostraTempo.setEditable(false);
+		
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;		
+		jpanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200,25));
+		gridBagConstraints.gridy++;
+		jpanel.add(mostraTempo2, gridBagConstraints);
+		mostraTempo2.setEditable(false);
+		
+		gridBagConstraints.gridwidth = 1;
+		
+		jbutton.setPreferredSize(new Dimension(92,25));
+		gridBagConstraints.gridy++;
+		jpanel.add(jbutton,gridBagConstraints);
+		
+		jbutton1.setPreferredSize(new Dimension(92,25));
+		gridBagConstraints.gridx++;
+		jpanel.add(jbutton1 , gridBagConstraints);
+		
+	
+		
+		
+		
+	
+		
+		
+		
+		
+		add(jpanel, BorderLayout.WEST);
+		setVisible(true);/*Torna a tela visivel para o usuario , sempre tem que ser o ultimo comando*/
+		
+	}
+
+}
